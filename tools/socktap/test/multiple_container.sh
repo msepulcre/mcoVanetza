099f2a994b8fc3813c3165aa2a7431d12c643416
YerayGuadalcazar <yeray.guadalcazar@goumh.umh.es>
2023-08-31 13:13:21 +0200

    Modificado multiple_continer.sh y Dockerfile para que los contenedores se comuniquen entre si

--- tools/socktap/test/multiple_container.sh
+++ tools/socktap/test/multiple_container.sh
@@ -12,8 +12,16 @@ for((i = 1; i <= $1; i++)); do
 
 done
 
-for((i = 1; i <= $1; i++)); do
-    
-    sudo docker run -ti -v /usr/local/src/socktap$1:/usr/local/src/socktap --name socktap$i --network bridge socktap-docker
+if [ "$1" == "1" ]; then
 
-done
+    sudo docker run -ti -v /usr/local/src/socktap1:/usr/local/src/socktap --name socktap --network bridge socktap-docker
+
+else
+
+    for((i = 1; i <= $1; i++)); do
+    
+        sudo docker run -d -v /usr/local/src/socktap$1:/usr/local/src/socktap --name socktap$i --network bridge socktap-docker
+
+    done
+
+fi
